a0b013beae1cad48b64aa1a121928b79e5a9e947
Felipe Borba <felipe.borba0109@gmail.com>
2022-07-17 00:41:32 -0300

    add styled components, global style and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { GlobalStyle } from "./styles/global";
+import { theme } from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
