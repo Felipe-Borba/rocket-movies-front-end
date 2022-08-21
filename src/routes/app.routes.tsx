3df9474fa907a42a6ca4eb1a1305714b48434186
Felipe Boba <felipe.borba0109@gmail.com>
2022-08-21 12:43:14 -0300

    feat: add route pages

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
+import MovieNew from '../pages/MovieNew';
 import { MoviePreview } from '../pages/MoviePreview';
+import Profile from '../pages/Profile';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<h1>TODO</h1>} />
-      <Route path="/movie/new" element={<h1>TODO</h1>} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/movie/new" element={<MovieNew />} />
       <Route path="/movie/:id" element={<MoviePreview />} />
     </Routes>
   );
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 export const AuthRoutes = () => {
   return (
     <Routes>
-      <Route path="/sign-in" element={<h1>TODO</h1>} />
-      <Route path="/sign-up" element={<h1>TODO</h1>} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 }
