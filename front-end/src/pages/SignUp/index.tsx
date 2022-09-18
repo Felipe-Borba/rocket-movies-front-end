dd6ccc1535d00dfab2d8f9c3a5946da40bcfb216
Felipe Boba <felipe.borba0109@gmail.com>
2022-09-18 11:51:30 -0300

    feat: add api to create user

--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,34 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
 import { AiOutlineLock, AiOutlineMail, AiOutlineUser } from "react-icons/ai";
-import { Button } from '../../components/buttons/Button';
-import { ButtonText } from '../../components/buttons/ButtonText';
-import { Input } from '../../components/Input';
-import SigInLayout from '../../components/layouts/SignInLayout';
+import { Button } from "../../components/buttons/Button";
+import { ButtonText } from "../../components/buttons/ButtonText";
+import { Input } from "../../components/Input";
+import SigInLayout from "../../components/layouts/SignInLayout";
 import { Link, useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  function handleSignUp(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSignUp(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    navigate("/home");
+
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then((response) => {
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/sig-in");
+      })
+      .catch((error) => {
+        if(error?.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Nao foi possível cadastrar!')
+        }
+      });
 
   }
 
   return (
     <SigInLayout sectionText="Crie sua conta">
       <form onSubmit={handleSignUp}>
-        <Input placeholder="Nome">
+        <Input
+          placeholder="Nome"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        >
           <AiOutlineUser />
         </Input>
 
-        <Input placeholder="E-mail">
+        <Input
+          placeholder="E-mail"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        >
           <AiOutlineMail />
         </Input>
 
-        <Input placeholder="Senha">
+        <Input
+          placeholder="Senha"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        >
           <AiOutlineLock />
         </Input>
+
         <Button type="submit">Cadastrar</Button>
       </form>
 
