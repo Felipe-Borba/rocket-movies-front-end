24012fed0f395be58d6735a9d2fa4e018ec7eaef
Felipe Boba <felipe.borba0109@gmail.com>
2022-10-02 16:15:36 -0300

    feat: redirect to login page

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 export const AuthRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/sign-in" />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
-}
+};
