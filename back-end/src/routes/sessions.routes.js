cc0dba7d9c9a24bed058727b88c1ae474c9ef3d3
Felipe Boba <felipe.borba0109@gmail.com>
2022-10-01 22:33:01 -0300

    refactor: move back end to another repo

--- back-end/src/routes/sessions.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const SessionController = require("../controllers/SessionController");
-const sessionController = new SessionController();
-
-const sessionRoutes = Router();
-sessionRoutes.post("/", sessionController.create);
-
-module.exports = sessionRoutes;
